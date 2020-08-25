4886195fafa433deb67989c4b2504f28189bbf30
Rebecca Hong <rebeccahongsf@gmail.com>
2020-08-24 21:04:03 -0700

    Added AllBooks graphql firebase query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,42 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = prop => {
+  console.log(prop)
+  return (
+    <Layout>
+      {prop.data.allBook.edges.map(edge => (
+        <div key={edge.node.id}>
+          <h2>
+            {edge.node.title} — <span>{edge.node.author.name}</span>
+          </h2>
+          <div>{edge.node.summary}</div>
+        </div>
+      ))}
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allBook {
+      edges {
+        node {
+          id
+          summary
+          title
+          author {
+            id
+            name
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage

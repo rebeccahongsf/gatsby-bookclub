bbe27e59a815e7cd19389f876b8483de451b6f85
Rebecca Hong <rebeccahongsf@gmail.com>
2020-09-08 19:42:07 -0700

    Added firebase config

---
+++ src/components/Firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBLHrlGkfX4v8RBaVxkL1k1fdMFmnsbi1k",
+  authDomain: "gatsby-bookclub-65904.firebaseapp.com",
+  databaseURL: "https://gatsby-bookclub-65904.firebaseio.com",
+  projectId: "gatsby-bookclub-65904",
+  storageBucket: "gatsby-bookclub-65904.appspot.com",
+  messagingSenderId: "1006279128308",
+  appId: "1:1006279128308:web:19ecf847ee1cd212305aa1",
+  measurementId: "G-LXRSZFCM8T",
+}
+
+export default firebaseConfig
